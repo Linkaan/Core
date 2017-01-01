f709064e92bf6efc329de0dd8e1e2f45357afc24
Linus Styrén <linus122xbb@gmail.com>
2017-01-01 23:43:54 +0100

    added network.h and fg_events_init

--- network.c
+++ network.c
@@ -1,7 +1,11 @@
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <errno.h>
 
 #include <event2/listener.h>
@@ -23,4 +27,40 @@ fg_event_cb (struct bufferevent *bev, short events, void *ctx)
 		log_error ("error in bufferevent");
 	if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR))
 		bufferevent_free (bev);
+}
+
+static void
+accept_conn_cb (struct evconnlistener *listener, evutil_socket_t fd,
+	struct sockaddr *address, int socklen, void *ctx)
+{
+	struct event_base *base;
+	struct bufferevent *bev;
+
+	base = evconnlistener_get_base (listener);
+	bev = bufferevent_socket_new (base, fd, BEV_OPT_CLOSE_ON_FREE);
+	bufferevent_setcb (bev, echo_read_cb, NULL, echo_event_cb, NULL);	
+	bufferevent_enable (bev, EV_READ | EV_WRITE);
+}
+
+static void
+accept_error_cb (struct evconnlistener *listener, void *ctx)
+{
+	int err;
+	struct event_base *base;
+
+	base = evconnlistener_get_base (listener);
+	err = EVUTIL_SOCKET_ERROR ();
+	fprintf (stderr, "Error when listening on events (%d): %s\n", err,
+		evutil_socket_error_to_string (err));
+	event_base_loopexit (base, NULL);
+}
+
+struct fg_events_data *
+fg_events_init (void (*callback)(char *))
+{
+	ssize_t s;
+	struct sockaddr_in sin;
+	struct fg_events_data itdata;
+
+	memset (&itdata, 0, sizeof (itdata));
 }
---
+++ network.h
@@ -0,0 +1,38 @@
+/*
+ *  network.h
+ *    The names of functions callable from within network
+ *****************************************************************************
+ *  This file is part of Fågelmataren, an embedded project created to learn
+ *  Linux and C. See <https://github.com/Linkaan/Fagelmatare>
+ *  Copyright (C) 2015-2016 Linus Styrén
+ *
+ *  Fågelmataren is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the Licence, or
+ *  (at your option) any later version.
+ *
+ *  Fågelmataren is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU General Public Licence
+ *  along with Fågelmataren.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************
+ */
+
+#ifndef _NETWORK_H_
+#define _NETWORK_H_
+
+struct fg_events_data {
+	struct event_base *base;
+    pthread_t          base_t;
+};
+
+/* Initialize libevent and add asynchronous event listener, register cb */
+extern struct fg_events_data *fg_events_init (void (*)(char *));
+
+/* Tear down event loop and cleanup */
+extern void fg_events_shutdown (struct *fg_events_data);
+
+#endif /* _NETWORK_H_ */
